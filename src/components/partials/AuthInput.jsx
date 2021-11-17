9889f48bec2438fc202addd9213f15209cc5b814
Yahia Zakaria <yahiazakaria3200@gmail.com>
2021-11-17 10:50:52 +0200

    handle login

--- src/components/partials/AuthInput.jsx
+++ src/components/partials/AuthInput.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React from "react";
 
-const AuthInput = ({ name, type, placeholder,value,setValue }) => {
-    return (
-        <input onChange={(e)=>setValue(e.target.value)} className="AuthInput" value={value}
-            type={type}
-            name={name}
-            placeholder={placeholder} />
-    );
-}
+const AuthInput = ({ name, type, placeholder, value, setValue }) => {
+  return (
+    <input
+      onChange={(e) => {
+        setValue(e.target.value);
+      }}
+      className="AuthInput"
+      value={value}
+      type={type}
+      name={name}
+      placeholder={placeholder}
+    />
+  );
+};
 
 export default AuthInput;
