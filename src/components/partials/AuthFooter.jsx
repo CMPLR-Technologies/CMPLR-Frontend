90b45d92b2b3387dd9da7d147ae34015b7792d86
Yahia Zakaria <yahiazakaria3200@gmail.com>
2021-11-18 00:17:48 +0200

    change sass structure

--- src/components/partials/AuthFooter.jsx
+++ src/components/partials/AuthFooter.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
+import React from "react";
 
 const AuthFooter = () => {
-    return ( 
-        <div className="AuthFooter">
-            <div className="AuthFooter__left">
-                <a href="/">Terms</a>
-                <a href="/">Privacy</a>
-                <a href="/">Jobs</a>
-                <a href="/">Support</a>
-            </div>
+  return (
+    <div className="AuthFooter">
+      <div className="AuthFooter__left">
+        <a href="/">Terms</a>
+        <a href="/">Privacy</a>
+        <a href="/">Jobs</a>
+        <a href="/">Support</a>
+      </div>
 
-            <div className="AuthFooter__right">
-                <a href="/">Posted py</a>
-            </div>
+      <div className="AuthFooter__right"></div>
+    </div>
+  );
+};
 
-        </div>
-    );
-}
- 
 export default AuthFooter;
