b73ed811ef243e9ec0c68ff19116c6a1e6b0bf4a
hazemkak <hazemkasim3@gmail.com>
2021-12-06 13:09:22 +0200

    adding the themes to create post

--- src/components/createPost/View.jsx
+++ src/components/createPost/View.jsx
@@ -1,38 +1,29 @@
-import React from 'react';
-import p0 from '../../assets/images/profile_pic0.png';
+import React, { useContext } from 'react';
 import OptionsCreatePost from './containers/OptionsCard';
+import { ThemeContext, themes } from '../../contexts/themeContext/ThemeContext';
+import ProfilePicContainer from './containers/ProfilePicContainer';
 
 export default function CreatePost() {
+    const theme = useContext(ThemeContext)[0];
+    const css = `
+        .create_container {
+            background: rgb(${themes[theme].white});
+        }
+        .profilepic_create{
+            color: rgb(${themes[theme].white});
+        }
+        .btncreateoption{
+            color: rgba(${themes[theme].black});
+        }
+    `;
     return (
         <>
+            <style>{css}</style>
             <div className="create1div">
                 <main className="create_main">
                     {/* */}
                     <div className="create_container">
-                        <div className="profilepic_create">
-                            <div className="profilepicinner_create">
-                                <span className="innerspan">
-                                    <span className="innerspan">
-                                        <a
-                                            className="inneranchor"
-                                            href="/user/prfile/TODO"
-                                        >
-                                            <div className="picontainer">
-                                                <div className="picStyles">
-                                                    <div className="picDiv">
-                                                        <img
-                                                            src={p0}
-                                                            alt="avatar"
-                                                            loading="eager"
-                                                        />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </span>
-                                </span>
-                            </div>
-                        </div>
+                        <ProfilePicContainer />
                         <OptionsCreatePost />
                     </div>
                 </main>
