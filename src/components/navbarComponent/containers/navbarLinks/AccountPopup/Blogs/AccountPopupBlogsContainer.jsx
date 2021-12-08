247b2b3cca7f93813124e25af177084e1f03ab71
hoskillua <47090776+hoskillua@users.noreply.github.com>
2021-12-08 11:56:09 +0200

    small fix (getting data from db)

--- src/components/navbarComponent/containers/navbarLinks/AccountPopup/Blogs/AccountPopupBlogsContainer.jsx
+++ src/components/navbarComponent/containers/navbarLinks/AccountPopup/Blogs/AccountPopupBlogsContainer.jsx
@@ -5,16 +5,17 @@ import axios from 'axios';
 export default function AccountPopupBlogsContainer() {
     const [blogs, setBlogs] = useState([]);
     useEffect(() => {
-        axios.get('http://localhost:3333/users').then(response => {
-            setBlogs(response.data.blogs);
+        axios.get('http://localhost:3333/blogs').then(response => {
+            setBlogs(response.data);
         });
     }, []);
 
     return (
         <div className="account-popup-blogs-container">
-            {blogs.map((blog, index) => (
-                <AccountPopupBlog blog={blog} key={index} />
-            ))}
+            {blogs &&
+                blogs.map((blog, index) => (
+                    <AccountPopupBlog blog={blog} key={index} />
+                ))}
         </div>
     );
 }
