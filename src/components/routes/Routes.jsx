deec965ff6c5f0d2707794644601589c9a5c9705
AhmedKhaled590 <kahmd1444@gmail.com>
2021-12-21 22:11:53 +0200

    solve lint Errors

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -12,7 +12,6 @@ import HandMadeTextEditor from '../RichTextEditor/View';
 import CreateModal from '../createPost/containers/PopupContainer/View';
 import Dashboard from '../dashboardComponent/View';
 
-import PostComponent from '../partials/postComponent/View';
 export default function MainRoutes() {
     const theme = useContext(ThemeContext)[0];
     const css = `
