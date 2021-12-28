75988ba1b3005282cde8d1d1bbd58babe05ec27a
Yahia Zakaria <yahiazakaria3200@gmail.com>
2021-12-28 21:46:03 +0200

    kaaak

--- src/components/explore/Data.js
+++ src/components/explore/Data.js
@@ -95,7 +95,7 @@ const followingHashtags = [
         name: '# code',
         link: '/tagged/code',
         image: 'https://64.media.tumblr.com/5321df071c0777b12faa659e7662e97a/adf9e8c81bd814f7-70/s640x960/0fb9219fd16d394613e0d8731b02ad44e9e22e4d.jpg',
-        posts: 69
+        posts: 70
     },
     {
         name: '# code',
