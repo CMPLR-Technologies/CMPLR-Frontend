f3ac8a8528fde19d8ba83cf15fade3106860d8da
AhmedKhaled590 <kahmd1444@gmail.com>
2021-12-29 04:38:13 +0200

    Edit post Componenet for ask

---
+++ src/components/partials/postComponent/containers/postTypesComponents/AskPost.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function AskPost(props) {
+    const { title, content } = props;
+    return (
+        <div className="ask-body">
+            <div className="ask-body-cont">
+                <div className="ask-content-sender">hazemabdo22 asked:</div>
+                <div className="ask-content">
+                    why are you gay? why are you gay? why are you gay? why are
+                    you gay? why are you gay? why are you gay? why are you gay?
+                    why are you gay? why are you gay? why are you gay? why are
+                    you gay? why are you gay? why are you gay? why are you gay?
+                    why are you gay? why are you gay? why are you gay? why are
+                    you gay? why are you gay? why are you gay? why are you gay?
+                    why are you gay? why are you gay? why are you gay?
+                </div>
+            </div>
+        </div>
+    );
+}
