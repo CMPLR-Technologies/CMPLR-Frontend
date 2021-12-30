27b1fa3c164cdaad270cd70af4a189cb79fcab3e
Hazem Abdo <ha4450770@gmail.com>
2021-12-30 06:44:10 +0200

    Fixing multiple blogs

--- src/components/blogSettingsComponent/Service.jsx
+++ src/components/blogSettingsComponent/Service.jsx
@@ -153,6 +153,8 @@ export function updatePropertyInDb(
                             blogName: propertyValue
                         })
                     );
+
+              
                 }
             }
         })
