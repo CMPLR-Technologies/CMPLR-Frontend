945db19c2f264dd6111b86095b647018c1029d85
hazemkak <hazemkasim3@gmail.com>
2021-12-30 19:53:12 +0200

    fixing chat file

--- src/components/chatComponent/Chat.test.js
+++ src/components/chatComponent/Chat.test.js
@@ -1,41 +1,45 @@
 /* eslint-disable no-undef */
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import MockedComponent from '../partials/MockedComponent';
 import ChatMessageItem from './containers/ChatMessageItem';
 import ChatOption from './containers/ChatOption';
 describe('test chatMessageItem', () => {
     it('test chat message name', () => {
         render(
-            <ChatMessageItem
+            <MockedComponent
+            component={<ChatMessageItem
                 name={'gaser'}
                 photo={'tst.jpg'}
                 message={'hi'}
                 link={'#'}
-            />
+            />}/>
         );
         const divElement = screen.getByText('gaser');
         expect(divElement).toBeInTheDocument();
     });
     it('test chat message message', () => {
-        render(
+        render(<MockedComponent
+            component={
             <ChatMessageItem
                 name={'gaser'}
                 photo={'tst.jpg'}
                 message={'hi'}
                 link={'#'}
-            />
+            />}/>
         );
         const divElement = screen.getByText('hi');
         expect(divElement).toBeInTheDocument();
     });
     it('test chat message img', () => {
-        render(
+        render(<MockedComponent
+            component={
             <ChatMessageItem
                 name={'gaser'}
                 photo={'tst.jpg'}
                 message={'hi'}
                 link={'#'}
-            />
+            />}/>
         );
         const imgElement = screen.getByAltText('gaser');
         expect(imgElement.src).toContain('tst.jpg');
@@ -44,17 +48,14 @@ describe('test chatMessageItem', () => {
 
 describe(' test chat option', () => {
     it('test chat option name', () => {
-        render(<ChatOption name={'gaser'} close={() => {}} />);
-        const divElement = screen.getByText('Block gaser');
-        expect(divElement).toBeInTheDocument();
-    });
-    it('test chat option name', () => {
-        render(<ChatOption name={'gaser'} close={() => {}} />);
+        render(<MockedComponent
+            component={<ChatOption name={'gaser'} close={() => {}} />}/>);
         const divElement = screen.getByText('Sound setting');
         expect(divElement).toBeInTheDocument();
     });
     it('test chat option name', () => {
-        render(<ChatOption name={'gaser'} close={() => {}} />);
+        render(<MockedComponent
+            component={<ChatOption name={'gaser'} close={() => {}} />}/>);
         const divElement = screen.getByText('Delete conversation');
         expect(divElement).toBeInTheDocument();
     });
--- src/components/partials/MockedComponent.jsx
+++ src/components/partials/MockedComponent.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import UserContextProvider from '../../contexts/userContext/UserContext';
+import { ThemeContextProvider } from '../../contexts/themeContext/ThemeContext';
+import ChatContextProvider from '../../contexts/chatContext/chatContext';
 
 export default function MockedComponent(props) {
     const { component } = props;
     return (
         <BrowserRouter>
-            <UserContextProvider>{component}</UserContextProvider>
+            <UserContextProvider>
+                <ThemeContextProvider>
+                    <ChatContextProvider>{component}</ChatContextProvider>
+                </ThemeContextProvider>
+            </UserContextProvider>
         </BrowserRouter>
     );
 }
